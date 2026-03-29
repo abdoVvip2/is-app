63de1e393f35b4871c06f70b43eeb5a359dacc7c
AbdoVvip2 <B34651515@gmail.com>
2026-03-29 22:11:00 +0100

    Correcting filename case for Linux runner

--- Main.js
+++
@@ -1,8 +0,0 @@
-cat <<EOF > main.js
-const { app, BrowserWindow } = require('electron');
-app.on('ready', () => {
-  const win = new BrowserWindow({ width: 1000, height: 800 });
-  win.setMenu(null); 
-  win.loadFile('index.html');
-});
-EOF
---
+++ main.js
@@ -0,0 +1,6 @@
+const { app, BrowserWindow } = require("electron");
+app.on("ready", () => {
+  const win = new BrowserWindow({ width: 1000, height: 800 });
+  win.setMenu(null);
+  win.loadFile("index.html");
+});
